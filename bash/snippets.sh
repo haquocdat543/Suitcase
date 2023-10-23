dd7137ffd7f73e39085adf2d3b3ded210cab65bc
haquocdat543 <wwwdatha543@gmail.com>
2023-10-23 14:40:57 +0000

    update snippets.sh

--- bash/snippets.sh
+++ bash/snippets.sh
@@ -6,3 +6,4 @@ then
 fi
 
 rm -f ~/preconfig/snippets/yaml.snippets && touch ~/preconfig/snippets/yaml.snippets && curl -s https://raw.githubusercontent.com/haquocdat543/Suitcase/main/snippets/yaml.snippets | sudo tee -a ~/preconfig/snippets/yaml.snippets
+rm -f ~/preconfig/snippets/snippets.snippets && touch ~/preconfig/snippets/snippets.snippets && curl -s https://raw.githubusercontent.com/haquocdat543/Suitcase/main/snippets/snippets.snippets | sudo tee -a ~/preconfig/snippets/snippets.snippets

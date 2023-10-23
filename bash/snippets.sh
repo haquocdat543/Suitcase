7e1aa403235ba313e75eaf97b0d2c86a5666d962
haquocdat543 <wwwdatha543@gmail.com>
2023-10-23 14:02:27 +0000

    fix snippets.sh

--- bash/snippets.sh
+++ bash/snippets.sh
@@ -5,7 +5,7 @@ then
 	mkdir ~/preconfig/snippets
 fi
 
-if [ ! -d ~/preconfig/snippets/yaml.snippets ]
+if [ ! -f ~/preconfig/snippets/yaml.snippets ]
 then
 	touch ~/preconfig/snippets/yaml.snippets && curl -s https://raw.githubusercontent.com/haquocdat543/Suitcase/main/snippets/yaml.snippets | sudo tee -a ~/preconfig/snippets/yaml.snippets
 fi

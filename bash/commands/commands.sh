668d559e6592a9cb8f617f5fb6817d0df0312ea0
haquocdat543 <wwwdatha543@gmail.com>
2023-10-25 01:49:37 +0000

    Update commands.sh

--- bash/commands/commands.sh
+++ bash/commands/commands.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-
+####	RECREATE FILE AND FOLDER	####
 ## If preconfig/commands/snippets.sh file exist. Delete and recreate it 
 	rm -f ~/preconfig/commands/snippets.sh && touch ~/preconfig/commands/snippets.sh && curl -s https://raw.githubusercontent.com/haquocdat543/Suitcase/main/bash/commands/snippets.sh | sudo tee -a ~/preconfig/commands/snippets.sh 
 
+####	FIX FILE AND FOLDER	####
 ## If preconfig/commands/lazygit.sh file exist. Delete and recreate it 
 if [ ! -f ~/preconfig/commands/lazygit.sh ]
 then

9e5c804ae413595464b09ad1c177830bafdf79d2
haquocdat543 <wwwdatha543@gmail.com>
2023-10-26 05:49:33 +0000

    fix k9s.sh

--- bash/commands/k9s.sh
+++ bash/commands/k9s.sh
@@ -2,7 +2,7 @@
 
 wget https://github.com/derailed/k9s/releases/download/v0.27.4/k9s_Linux_amd64.tar.gz
 tar xvf k9s_Linux_amd64.tar.gz
-mv k9s /usr/local/k9s
+mv k9s /usr/local/bin/k9s
 rm -f k9s_Linux_amd64.tar.gz
 k9s --version
 

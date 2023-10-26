2107e09cf79b4ab3a6a92ff080da8610e93b813f
haquocdat543 <wwwdatha543@gmail.com>
2023-10-26 04:42:01 +0000

    add dock8s.md

--- bash/docs/docs.sh
+++ bash/docs/docs.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 ####	RECREATE FILE AND FOLDER	####
 ## If preconfig/commands/snippets.sh file exist. Delete and recreate it 
-#	rm -f ~/preconfig/commands/snippets.sh && touch ~/preconfig/commands/snippets.sh && curl -s https://raw.githubusercontent.com/haquocdat543/Suitcase/main/bash/commands/snippets.sh | sudo tee -a ~/preconfig/commands/snippets.sh 
-
+##	rm -f ~/preconfig/commands/snippets.sh && touch ~/preconfig/commands/snippets.sh && curl -s https://raw.githubusercontent.com/haquocdat543/Suitcase/main/bash/commands/snippets.sh | sudo tee -a ~/preconfig/commands/snippets.sh 
 ####	FIX FILE AND FOLDER	####
 ## If preconfig/docs/dock8s.md file exist. Delete and recreate it 
 if [ ! -f ~/preconfig/docs/dock8s.md ]

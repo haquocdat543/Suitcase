2f2ea8329752162fe8ec092b08e5c1533f14fd5b
haquocdat543 <wwwdatha543@gmail.com>
2023-11-03 07:13:09 +0000

    Update alias

--- bash/bash.sh
+++ bash/bash.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-bash <(curl -s https://raw.githubusercontent.com/haquocdat543/Suitcase/main/bash/alias.sh );
-up2;
-rv;
-plug;
-yg;
-yn;
-cm;
-sn;
+bash <(curl -s https://raw.githubusercontent.com/haquocdat543/Suitcase/main/bash/alias.sh )
+up2
+rv
+plug
+yg
+yn
+cm
+sn
 

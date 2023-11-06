6ea58a89a85d61d1e80460d213bc03d83717ce6d
haquocdat543 <wwwdatha543@gmail.com>
2023-11-06 05:44:30 +0000

    Update zellij commands 3

--- bash/commands/zellijInstall.sh
+++ bash/commands/zellijInstall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 curl -L -o zellij.tar.gz https://github.com/zellij-org/zellij/releases/download/v0.38.2/zellij-x86_64-unknown-linux-musl.tar.gz
 tar xzvf zellij.tar.gz zellij
-cp ./zellij /usr/local/bin/zellij
+mv ./zellij /usr/local/bin/zellij
 rm zellij.tar.gz

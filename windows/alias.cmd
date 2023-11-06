3e4ae26d8e48a2c605eb721e0a3e323f495a0739
haquocdat543 <wwwdatha543@gmail.com>
2023-11-06 14:48:00 +0000

    add windows folder ( alias.cmd, .reg )

---
+++ windows/alias.cmd
@@ -0,0 +1,24 @@
+@echo off
+
+:: Temporary system path at cmd startup
+
+set PATH=%PATH%;"C:\Program Files\Sublime Text 2\"
+
+:: Add to path by command
+
+DOSKEY add_python26=set PATH=%PATH%;"C:\Python26\"
+DOSKEY add_python33=set PATH=%PATH%;"C:\Python33\"
+
+:: Commands
+
+DOSKEY ls=dir /B $*
+DOSKEY sublime=sublime_text $*  
+    ::sublime_text.exe is name of the executable. By adding a temporary entry to system path, we don't have to write the whole directory anymore.
+DOSKEY gsp="C:\Program Files (x86)\Sketchpad5\GSP505en.exe"
+DOSKEY alias=notepad %USERPROFILE%\Dropbox\alias.cmd
+
+:: Common directories
+
+DOSKEY dropbox=cd "%USERPROFILE%\Dropbox\$*"
+DOSKEY research=cd %USERPROFILE%\Dropbox\Research\
+

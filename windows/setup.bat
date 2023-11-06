001be3c6e69a7d7edd503458c3ec985934ccaaae
haquocdat543 <wwwdatha543@gmail.com>
2023-11-06 15:08:27 +0000

    Update setup.bat

---
+++ windows/setup.bat
@@ -0,0 +1,3 @@
+curl -L -o %USERPROFILE%/.reg https://raw.githubusercontent.com/haquocdat543/Suitcase/main/windows/.reg
+curl -L -o %USERPROFILE%/alias.cmd https://raw.githubusercontent.com/haquocdat543/Suitcase/main/windows/alias.cmd
+%USERPROFILE%/.reg

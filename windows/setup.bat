566f62b4533f8b432d268db6a71b0a38c12b4577
haquocdat543 <wwwdatha543@gmail.com>
2023-11-06 15:48:12 +0000

    Update setup.bat

--- windows/setup.bat
+++ windows/setup.bat
@@ -1,3 +1,6 @@
 curl -L -o %USERPROFILE%/.reg https://raw.githubusercontent.com/haquocdat543/Suitcase/main/windows/.reg
 curl -L -o %USERPROFILE%/alias.cmd https://raw.githubusercontent.com/haquocdat543/Suitcase/main/windows/alias.cmd
 %USERPROFILE%/.reg
+Set-ExecutionPolicy AllSigned
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+choco install git.install

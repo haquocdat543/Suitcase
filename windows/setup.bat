9bb3128509dbc0db671d0e36c871779baa84c952
haquocdat543 <wwwdatha543@gmail.com>
2023-11-09 13:46:55 +0000

    Update setup.bat

--- windows/setup.bat
+++ windows/setup.bat
@@ -5,7 +5,7 @@ curl -L -o %USERPROFILE%/.tmux.conf https://raw.githubusercontent.com/haquocdat5
 curl -L -o %USERPROFILE%/Desktop/connect.bat https://raw.githubusercontent.com/haquocdat543/Suitcase/main/windows/connect.bat
 curl -L -o %USERPROFILE%/Desktop/install.ps1 https://raw.githubusercontent.com/haquocdat543/Suitcase/main/windows/install.ps1
 curl -L -o %USERPROFILE%/Desktop/powershell.txt https://raw.githubusercontent.com/haquocdat543/Suitcase/main/windows/powershell.txt
-IF not exist %USERPROFILE%/preconfig ( mkdir %USERPROFILE%/preconfig )
-IF not exist %USERPROFILE%/.vim ( mkdir %USERPROFILE%/.vim )
-IF not exist %USERPROFILE%/.vim/autoload ( mkdir %USERPROFILE%/.vim/autoload )
+IF not exist %USERPROFILE%/preconfig mkdir %USERPROFILE%/preconfig 
+IF not exist %USERPROFILE%/.vim mkdir %USERPROFILE%/.vim 
+IF not exist %USERPROFILE%/.vim/autoload mkdir %USERPROFILE%/.vim/autoload 
 %USERPROFILE%/.reg

85987134d82c4ed10727a041bf35e11cb632243f
haquocdat543 <wwwdatha543@gmail.com>
2023-11-09 11:34:01 +0000

    Update window folder
    Update setup.bat

--- windows/setup.bat
+++ windows/setup.bat
@@ -1,5 +1,7 @@
 curl -L -o %USERPROFILE%/.reg https://raw.githubusercontent.com/haquocdat543/Suitcase/main/windows/.reg
 curl -L -o %USERPROFILE%/alias.cmd https://raw.githubusercontent.com/haquocdat543/Suitcase/main/windows/alias.cmd
+curl -L -o %USERPROFILE%/.vimrc https://raw.githubusercontent.com/haquocdat543/Suitcase/main/bash/.vimrc
+curl -L -o %USERPROFILE%/.tmux.conf https://raw.githubusercontent.com/haquocdat543/Suitcase/main/bash/.tmux.conf
 curl -L -o %USERPROFILE%/Desktop/connect.bat https://raw.githubusercontent.com/haquocdat543/Suitcase/main/windows/connect.bat
 curl -L -o %USERPROFILE%/Desktop/install.ps1 https://raw.githubusercontent.com/haquocdat543/Suitcase/main/windows/install.ps1
 curl -L -o %USERPROFILE%/Desktop/powershell.txt https://raw.githubusercontent.com/haquocdat543/Suitcase/main/windows/powershell.txt

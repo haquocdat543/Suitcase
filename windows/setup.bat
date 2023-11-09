649186777885749bed503b954f33f03fafc019f0
haquocdat543 <wwwdatha543@gmail.com>
2023-11-09 07:15:36 +0000

    Update window folder 6
    Modified path in setup.bat

--- windows/setup.bat
+++ windows/setup.bat
@@ -1,5 +1,5 @@
 curl -L -o %USERPROFILE%/.reg https://raw.githubusercontent.com/haquocdat543/Suitcase/main/windows/.reg
 curl -L -o %USERPROFILE%/alias.cmd https://raw.githubusercontent.com/haquocdat543/Suitcase/main/windows/alias.cmd
-curl -L -o %USERPROFILE%/Downloads/connect.bat https://raw.githubusercontent.com/haquocdat543/Suitcase/main/windows/connect.bat
-curl -L -o %USERPROFILE%/Downloads/install.ps1 https://raw.githubusercontent.com/haquocdat543/Suitcase/main/windows/install.ps1
+curl -L -o %USERPROFILE%/Desktop/connect.bat https://raw.githubusercontent.com/haquocdat543/Suitcase/main/windows/connect.bat
+curl -L -o %USERPROFILE%/Desktop/install.ps1 https://raw.githubusercontent.com/haquocdat543/Suitcase/main/windows/install.ps1
 %USERPROFILE%/.reg

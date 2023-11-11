f349310a0d6715ffe84fe089648a57f87d8eb596
haquocdat543 <wwwdatha543@gmail.com>
2023-11-11 12:11:35 +0000

    Update lazygit.sh and .bashrc

--- bash/commands/lazygit.sh
+++ bash/commands/lazygit.sh
@@ -4,4 +4,4 @@ export VER="0.40.2"
 wget -O lazygit.tgz https://github.com/jesseduffield/lazygit/releases/download/v${VER}/lazygit_${VER}_Linux_x86_64.tar.gz
 tar xvf lazygit.tgz
 sudo mv lazygit /usr/local/bin/
-rm LISENCE README.md lazygit.tgz
+rm LICENSE README.md lazygit.tgz

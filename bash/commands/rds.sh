1bc90170717ab89cc6b07326d9fffcbe29fffc68
haquocdat543 <wwwdatha543@gmail.com>
2023-11-17 13:08:48 +0000

    Update rds bash

--- bash/commands/rds.sh
+++ bash/commands/rds.sh
@@ -6,11 +6,11 @@ read -p "Enter class [db.t3.micro]:" class
 class=${class:-db.t3.micro}
 read -p "Enter engine [mysql]:" engine
 engine=${engine:-mysql}
-read -p "Enter masterUsername [admin]:" masterUsername
+read -p "Enter master username [admin]:" masterUsername
 masterUsername=${masterUsername:-admin}
 read -p "Enter master password [adminsecret]:" password
 password=${password:-adminsecret}
-read -p "Enter allocate-storage [20]:" storage
+read -p "Enter allocate storage [20]:" storage
 storage=${storage:-20}
 aws rds create-db-instance --db-instance-identifier $identifier --db-instance-class $class --engine $engine --master-username $masterUsername --master-user-password $password --allocated-storage $storage
 

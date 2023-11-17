9268857c59f498d659f7866ffd37d8db9d6707d1
haquocdat543 <wwwdatha543@gmail.com>
2023-11-17 15:09:13 +0000

    Update agInstall

--- bash/commands/agInstall.sh
+++ bash/commands/agInstall.sh
@@ -3,6 +3,9 @@
 sudo yum install -y pcre-devel
 sudo yum install -y xz-devel
 sudo yum install -y zlib-devel
+sudo yum install -y automake
+sudo yum install -y make
+sudo yum install -y gcc
 cd /usr/local/src
 sudo git clone https://github.com/ggreer/the_silver_searcher.git
 cd the_silver_searcher

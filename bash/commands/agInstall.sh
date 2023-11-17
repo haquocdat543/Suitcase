448f5eb6cc2f8bfdd7a1dd8db8eacfa8b485dd6c
haquocdat543 <wwwdatha543@gmail.com>
2023-11-17 15:13:00 +0000

    Update agInstall

--- bash/commands/agInstall.sh
+++ bash/commands/agInstall.sh
@@ -11,6 +11,6 @@ sudo git clone https://github.com/ggreer/the_silver_searcher.git
 cd the_silver_searcher
 sudo ./build.sh
 sudo make install
-	
-
+cp -f /usr/local/src/the_silver_searcher/ag /usr/local/bin/ag
+cd -
 

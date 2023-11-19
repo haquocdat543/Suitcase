e740d7e778d9c640c77eab938bd65836428e6767
haquocdat543 <wwwdatha543@gmail.com>
2023-11-19 07:44:33 +0000

    Update init.lua

--- bash/init.lua
+++ bash/init.lua
@@ -1,1 +1,21 @@
-require'lspconfig'.pyright.setup{}
+vim.cmd('autocmd!')
+
+vim.opt.title = true
+vim.opt.hlsearch = true
+vim.opt.history = 200
+vim.opt.bg = dark
+vim.opt.paste = true
+vim.opt.wrap = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.ruler = true
+vim.opt.showcmd = true
+vim.opt.showmode = true
+vim.opt.wildmenu = true
+vim.opt.laststatus = 2
+vim.opt.cursorline = true
+
+local keymap = vim.keymap
+
+keymap.set('n','+','<C-a>')
+keymap.set('n','-','<C-x>')

ff9d8d9bde504bdbac4534f074b1dd9473db8744
haquocdat543 <wwwdatha543@gmail.com>
2023-11-19 09:13:45 +0000

    Update init.lua

--- bash/init.lua
+++ bash/init.lua
@@ -22,6 +22,29 @@ keymap.set('n','+','<C-a>')
 keymap.set('n','-','<C-x>')
 keymap.set('n','<C-a>','gg<M-v>G')
 keymap.set('n','<leader>ii',':PackerInstall<CR>')
+keymap.set('n','<leader>ff',':Files<CR>')
+keymap.set('n','<leader>fg',':Rg<CR>')
+keymap.set('n','<leader>fb',':Buffers<CR>')
+keymap.set('n','<leader>ft',':FloatermNew<CR>')
+keymap.set('n','<leader>nn',':set nopaste<CR>')
+keymap.set('n','<leader>nm',':set paste<CR>')
+keymap.set('n',',tt',':ter<CR>')
+keymap.set('n',',gb',':Git blame<CR>')
+keymap.set('n','<C-b>b',':set backup<CR>')
+keymap.set('n','<C-h>b',':noh<CR>')
+keymap.set('n','<C-q>q',':q!<CR>')
+keymap.set('n','<C-q>w',':wq!<CR>')
+keymap.set('n','<C-c>1',':%s/')
+keymap.set('n','<C-c>2',':.s/')
+keymap.set('n','<C-c>3',':.,0/')
+keymap.set('n','<C-c>4',':.,$/')
+keymap.set('n','<C-c>5',':.,.-')
+keymap.set('n','<C-c>6',':.,.+')
+keymap.set('n','<C-x>','<C-p><CR>')
+keymap.set('n','<C-a>a',':UltiSnipsEdit<CR>')
+keymap.set('n','<A-w>w',':w!<CR>')
+keymap.set('n','<A-n>n',':n<CR>')
+keymap.set('n','<A-p>p',':p<CR>')
 
 keymap.set('n','te',':tabedit ')
 keymap.set('n','ss',':split<CR><C-w>w')
@@ -42,8 +65,28 @@ vim.cmd [[packadd packer.nvim]]
 packer.startup(function(use)
    use 'wbthomason/packer.nvim'
    use 'ellisonleao/gruvbox.nvim'
+   use { 'junegunn/fzf', run = ":call fzf#install()" }
+   use { 'junegunn/fzf.vim' }
+   use 'easymotion/vim-easymotion'
+   use {
+   'nvim-tree/nvim-tree.lua',
+   requires = {
+    'nvim-tree/nvim-web-devicons', -- optional
+     },
+   }
+   use 'vim-airline/vim-airline' 
+   use 'voldikss/vim-floaterm'
+   use 'SirVer/ultisnips'
+   use 'dinhhuy258/git.nvim'
+   use 'tpope/vim-fugitive'
+   use 'airblade/vim-gitgutter'
 end)
 
 vim.o.background = "dark" -- or "light" for light mode
 vim.cmd([[colorscheme gruvbox]])
-
+vim.cmd([[let g:UltiSnipsExpandTrigger="<tab>"]])
+vim.cmd([[let g:UltiSnipsListSnippets="<c-y>"]])
+vim.cmd([[let g:UltiSnipsJumpForwardTrigger="<c-m>"]])
+vim.cmd([[let g:UltiSnipsJumpBackwardTrigger="<c-n>"]])
+vim.cmd([[let g:UltiSnipsEditSplit="vertical"]])
+vim.cmd([[let g:UltiSnipsSnippetDirectories=['~/preconfig/snippets'] ]])

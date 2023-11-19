b79005f74d6cc946e11aa42e2e87836e0a2b5b4f
haquocdat543 <wwwdatha543@gmail.com>
2023-11-19 08:09:46 +0000

    Update init.lua

--- bash/init.lua
+++ bash/init.lua
@@ -1,9 +1,10 @@
 vim.cmd('autocmd!')
 
+vim.g.mapleader = ' '
 vim.opt.title = true
 vim.opt.hlsearch = true
 vim.opt.history = 200
-vim.opt.bg = dark
+vim.opt.background = 'dark'
 vim.opt.paste = true
 vim.opt.wrap = true
 vim.opt.number = true
@@ -19,3 +20,25 @@ local keymap = vim.keymap
 
 keymap.set('n','+','<C-a>')
 keymap.set('n','-','<C-x>')
+keymap.set('n','<C-a>','gg<M-v>G')
+
+keymap.set('n','te',':tabedit ')
+keymap.set('n','ss',':split<CR><C-w>w')
+keymap.set('n','sv',':vsplit<CR><C-w>w')
+keymap.set('n','sh','<C-w>h')
+keymap.set('n','sj','<C-w>j')
+keymap.set('n','sk','<C-w>k')
+keymap.set('n','sl','<C-w>l')
+
+local status, packer = pcall(require, 'packer')
+if (not status) then
+   print('Packer is not installed')
+   return
+end
+
+vim.cmd [[packadd packer.nvim]]
+
+packer.startup(function(use)
+   use 'wbthomason/packer.nvim'
+end)
+

549ffc81034689988e3894eace31632b26344fc7
haquocdat543 <wwwdatha543@gmail.com>
2023-11-20 08:36:30 +0000

    Uodate init.lua
    Add lspsaga config.

--- bash/init.lua
+++ bash/init.lua
@@ -418,6 +418,26 @@ cmp.setup({
       },
 })
 
+-- import lspsaga safely
+local saga_status, saga = pcall(require, "lspsaga")
+if not saga_status then
+  return
+end
+
+saga.setup({
+  -- keybinds for navigation in lspsaga window
+  scroll_preview = { scroll_down = "<C-f>", scroll_up = "<C-b>" },
+  -- use enter to open file with definition preview
+  definition = {
+    edit = "<CR>",
+  },
+  ui = {
+    colors = {
+      normal_bg = "#022746",
+    },
+  },
+})
+
 vim.o.background = "dark" -- or "light" for light mode
 vim.cmd([[colorscheme gruvbox]])
 vim.cmd([[let g:UltiSnipsExpandTrigger="<tab>"]])

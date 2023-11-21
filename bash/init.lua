c206aefe98c3c651f13a67da4f476d66637c83b7
haquocdat543 <wwwdatha543@gmail.com>
2023-11-21 03:09:37 +0000

    ** init.lua
    @@ lazygit

--- bash/init.lua
+++ bash/init.lua
@@ -43,6 +43,7 @@ keymap.set('n','<leader>nn',':set nopaste<CR>')
 keymap.set('n','<leader>nm',':set paste<CR>')
 keymap.set('n','<leader>mm',':Mason<CR>')
 keymap.set('n','<leader>ll',':Lazy<CR>')
+keymap.set('n','<leader>lg',':LazyGit<CR>')
 keymap.set('n','<leader>ch',':checkhealth ')
 keymap.set('n','<leader>mi',':MasonInstall ')
 keymap.set('n','<leader>gs',':G status<CR>')
@@ -90,6 +91,7 @@ keymap.set('n','sl','<C-w>l')
 
 local plugins = {
  'wbthomason/packer.nvim',
+ 'kdheepak/lazygit.nvim',
  'ryanoasis/vim-devicons',
  'Yggdroot/indentLine',
  'ellisonleao/gruvbox.nvim',
@@ -437,5 +439,11 @@ vim.cmd([[let g:gitgutter_sign_modified = '*' ]])
 vim.cmd([[let g:gitgutter_sign_removed = 'x' ]])
 vim.cmd([[let g:gitgutter_sign_removed_first_line = '^' ]])
 vim.cmd([[let g:gitgutter_sign_modified_removed = '<' ]])
+vim.cmd([[let g:lazygit_floating_window_winblend = 0 " transparency of floating window ]])
+vim.cmd([[let g:lazygit_floating_window_scaling_factor = 0.9 " scaling factor for floating window ]])
+vim.cmd([[let g:lazygit_floating_window_border_chars = ['╭','─', '╮', '│', '╯','─', '╰', '│'] " customize lazygit popup window border characters ]])
+vim.cmd([[let g:lazygit_floating_window_use_plenary = 0 " use plenary.nvim to manage floating window if available ]])
+vim.cmd([[let g:lazygit_use_neovim_remote = 1 " fallback to 0 if neovim-remote is not installed ]])
 
-
+vim.cmd([[let g:lazygit_use_custom_config_file_path = 0 " config file path is evaluated if this value is 1 ]])
+vim.cmd([[let g:lazygit_config_file_path = '' " custom config file path ]])

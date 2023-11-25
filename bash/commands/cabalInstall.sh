7810076f00a3dd3359d7ff79ff0b20c4db775550
haquocdat543 <wwwdatha543@gmail.com>
2023-11-25 13:26:46 +0000

    Update init.lua

--- bash/commands/cabalInstall.sh
+++ bash/commands/cabalInstall.sh
@@ -4,6 +4,6 @@ export VER="3.10.1.0"
 cd
 wget https://downloads.haskell.org/~cabal/cabal-install-${VER}/cabal-install-${VER}-x86_64-linux-centos7.tar.xz
 tar -xvf cabal-install-${VER}-x86_64-linux-centos7.tar.xz
-mv -f ./cabal /user/local/bin/cabal
+mv -f ~/cabal /user/local/bin/cabal
 rm -f cabal-install-${VER}-x86_64-linux-centos7.tar.xz
 cd -

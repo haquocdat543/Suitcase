c566d3ff6b87cc06fd485abb3cff069f9d923a07
haquocdat543 <wwwdatha543@gmail.com>
2023-11-25 13:47:03 +0000

    Update Cabal

--- bash/commands/cabalInstall.sh
+++ bash/commands/cabalInstall.sh
@@ -4,6 +4,9 @@ export VER="3.10.1.0"
 cd
 wget https://downloads.haskell.org/~cabal/cabal-install-${VER}/cabal-install-${VER}-x86_64-linux-centos7.tar.xz
 tar -xvf cabal-install-${VER}-x86_64-linux-centos7.tar.xz
-mv -f ~/cabal /user/local/bin/cabal
+sudo chown root ./cabal
+sudo chgrp root ./cabal
+sudo chmod 777 ./cabal
+mv ./cabal /usr/local/bin/cabal
 rm -f cabal-install-${VER}-x86_64-linux-centos7.tar.xz
 cd -

bf997836f90a91f0473e1917742e21ec82575175
haquocdat543 <wwwdatha543@gmail.com>
2023-11-27 13:14:51 +0000

    Update addAutoSuggest

--- bash/commands/addSudoers.sh
+++ bash/commands/addSudoers.sh
@@ -2,6 +2,4 @@
 
 read -p "Enter username to add to sudoers [ec2-user]: " username
 username=${username:-ec2-user}
-sudo sed "/root\t/a$username\tALL=(ALL)\tALL" /etc/sudoers | sudo tee /etc/sudoers
-
-
+sed -i "1s/^/$username\tALL=(ALL)\tALL\n/" /etc/sudoers

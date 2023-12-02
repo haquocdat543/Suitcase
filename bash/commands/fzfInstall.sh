279bcfd9cc4846caa9ed71f9344f6884bf80d573
haquocdat543 <wwwdatha543@gmail.com>
2023-12-02 08:05:11 +0000

    Update command

--- bash/commands/fzfInstall.sh
+++ bash/commands/fzfInstall.sh
@@ -2,5 +2,5 @@
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf 
 ~/.fzf/install
-sed -i '$ d' ~/.bashrc
+sed -i "$ d" ~/.bashrc
 sed -i "1s/^/[ -f ~\/.fzf.bash ] \&\& source ~\/.fzf.bash\n/" ~/.bashrc

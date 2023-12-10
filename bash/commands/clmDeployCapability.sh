0725610e2e62bcc05be10631b8012702ccf425fb
haquocdat543 <wwwdatha543@gmail.com>
2023-12-10 04:26:32 +0000

    Add clmCap

---
+++ bash/commands/clmDeployCapability.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+read -p "Enter your stack-name [default]: " stackname
+stackname=${stackname:-default}
+read -p "Enter your template-file path [k8s.yaml]: " templatefile
+templatefile=${templatefile:-k8s.yaml}
+aws cloudformation deploy --stack-name $stackname --template-file $templatefile --capabilities CAPABILITY_IAM
+

ff067cb8c6ebd00df7ac53c04add4ae14e9d7170
haquocdat543 <wwwdatha543@gmail.com>
2023-12-14 01:36:39 +0000

    Update glab version and add color to sh snippet

--- bash/commands/gitlabCliInstall.sh
+++ bash/commands/gitlabCliInstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VER="1.35.0"
+VER="1.36.0"
 cd
 wget https://gitlab.com/gitlab-org/cli/-/releases/v${VER}/downloads/glab_${VER}_Linux_x86_64.tar.gz
 tar -xvzf glab_${VER}_Linux_x86_64.tar.gz

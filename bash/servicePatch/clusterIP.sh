22e14ac38f49968e11b004984a965e1d535d5a65
haquocdat543 <wwwdatha543@gmail.com>
2023-12-16 15:37:42 +0000

    Update service Patch

--- bash/servicePatch/clusterIP.sh
+++ bash/servicePatch/clusterIP.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo "enter namespace name"
-read namespace
-echo "enter service name "
-read serviceName
-echo " $namespace and $serviceName "
-kubectl patch svc $serviceName -n $namespace -p '{"spec": {"type": "ClusterIP"}}'
-kubectl get service -n $namespace | grep $serviceName
+read -p "$(echo -e "Enter ${yellow}namespace ${purple}name${nc} [${green2}default${nc}]: ")" namespace
+namespace=${namespace:-default}
+read -p "$(echo -e "Enter ${yellow}service ${purple}name${nc} [${green2}argocd-server${nc}]: ")" service
+service=${service:-argocd-server}
+echo " $namespace and $service "
+kubectl patch svc $service -n $namespace -p '{"spec": {"type": "ClusterIP"}}'
+kubectl get service -n $namespace | grep $service

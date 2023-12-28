1ab262e0f23c694b091ad8f2bd9bd09d195ff25a
haquocdat543 <wwwdatha543@gmail.com>
2023-12-28 03:51:23 +0000

    Fix go install

--- bash/commands/goInstall.sh
+++ bash/commands/goInstall.sh
@@ -3,7 +3,8 @@
 export VER="1.21.3"
 wget https://go.dev/dl/go${VER}.linux-amd64.tar.gz
 rm -rf /usr/local/go && tar -C /usr/local -xzf go${VER}.linux-amd64.tar.gz
-sed -i "1s/^/export PATH=\$PATH:/usr/local/go/bin\n/" ~/.bashrc
+sed -i "1s/^/export PATH=\$PATH:\/usr\/local\/go\/bin\n/" ~/.bashrc
 export PATH=$PATH:/usr/local/go/bin
 rm -f go${VER}.linux-amd64
+rm -f go${VER}.linux-amd64.tar.gz
 go version

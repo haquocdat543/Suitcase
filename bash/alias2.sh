d981abf6ae5e1004ec461889f5c0a0375509ca99
haquocdat543 <wwwdatha543@gmail.com>
2024-01-14 03:00:21 +0000

    Add alias2.sh

---
+++ bash/alias2.sh
@@ -0,0 +1,16 @@
+yum install -y git
+apt install -y git
+git clone git@github.com/haquocdat543/Suitcase.git ~/Suitcase
+cd ~/Suitcase
+git reset --hard HEAD~1
+git reset --hard HEAD~1
+git reset --hard HEAD~1
+git reset --hard HEAD~1
+git reset --hard HEAD~1
+git reset --hard HEAD~1
+git reset --hard HEAD~1
+git reset --hard HEAD~1
+git reset --hard HEAD~1
+git reset --hard HEAD~1
+git pull origin main -f
+cd -

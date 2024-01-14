62d7f3cc91e488c496e308609c42b632eab06f26
haquocdat543 <wwwdatha543@gmail.com>
2024-01-14 00:42:30 +0000

    Update fzfInstall

--- bash/commands/fzfInstall.sh
+++ bash/commands/fzfInstall.sh
@@ -4,3 +4,4 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 sed -i "$ d" ~/.bashrc
 sed -i "1s/^/[ -f ~\/.fzf.bash ] \&\& source ~\/.fzf.bash\n/" ~/.bashrc
+sed -i "1s/^/export FZF_CTRL_T_OPTS\=\"--preview\='less {}' --bind shift-up:preview-page-up,shift-down:preview-page-down\"\n/" ~/.bashrc

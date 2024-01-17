fdedacdbe05465670d17b5e216c2b690285b9381
haquocdat543 <wwwdatha543@gmail.com>
2024-01-17 02:56:35 +0000

    Fix gcpCli

--- bash/commands/gcpCli.sh
+++ bash/commands/gcpCli.sh
@@ -4,9 +4,7 @@ export VER="455.0.0"
 cd
 curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-${VER}-linux-x86_64.tar.gz
 tar -xf google-cloud-cli-${VER}-linux-x86_64.tar.gz
-./google-cloud-sdk/install.sh
-./google-cloud-sdk/install.sh --help
-./google-cloud-sdk/bin/gcloud init
+~/google-cloud-sdk/install.sh
+~/google-cloud-sdk/bin/gcloud init
 rm -f google-cloud-cli-${VER}-linux-x86_64.tar.gz
-rm -rf google-cloud-sdk
 cd -

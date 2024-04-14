7317b26bfad4aef4380a759749b7506cf89a1f98
haquocdat543 <wwwdatha543@gmail.com>
2024-04-14 19:58:23 +0700

    Upgrade aws sdk delete ec2 instance with input

--- bash/code/aws/jsv3/deleteEc2Instance.js
+++ bash/code/aws/jsv3/deleteEc2Instance.js
@@ -1,4 +1,5 @@
 const { EC2Client, TerminateInstancesCommand } = require("@aws-sdk/client-ec2");
+const { createInterface } = require('readline');
 
 // Replace with your AWS credentials
 const credentials = {
@@ -7,19 +8,50 @@ const credentials = {
   secretAccessKey: "",
 };
 
-const ec2Client = new EC2Client(credentials);
-
-const instanceId = "i-0c049d905ec11b8da"; // Replace with the ID of the instance to terminate
-
-const terminateInstancesCommand = new TerminateInstancesCommand({
-  InstanceIds: [instanceId],
+const rl = createInterface({
+  input: process.stdin,
+  output: process.stdout
 });
 
-ec2Client.send(terminateInstancesCommand)
-  .then((data) => {
-    console.log("Termination successful");
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+async function terminateInstance() {
+  try {
+    const ec2Client = new EC2Client(credentials);
+
+    // Get instance ID from user input
+    const question = "Enter the ID of the instance to terminate: ";
+    const answer = await new Promise((resolve) => rl.question(question, resolve));
+
+    if (!answer) {
+      console.error("Please enter an instance ID.");
+      return;
+    }
+
+    // Confirmation prompt for safety
+    const confirmQuestion = `Are you sure you want to terminate instance with ID ${answer} (y/N)? `;
+    const confirmation = await new Promise((resolve) => rl.question(confirmQuestion, resolve));
+
+    if (confirmation.toLowerCase() !== 'y') {
+      console.log("Termination cancelled.");
+      return;
+    }
+
+    const terminateInstancesCommand = new TerminateInstancesCommand({
+      InstanceIds: [answer],
+    });
+
+    await ec2Client.send(terminateInstancesCommand);
+    console.log(`Instance with ID ${answer} termination initiated.`);
+  } catch (error) {
+    if (error.code === 'InvalidInstanceID.NotFound') {
+      console.error(`Instance with ID ${answer} not found.`);
+    } else {
+      console.error("Termination failed:", error);
+    }
+  } finally {
+    rl.close();
+  }
+}
+
+terminateInstance();
+
 

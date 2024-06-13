5be4ac8f658adb6d79911ccfdddae1684b9968f5
haquocdat543 <wwwdatha543@gmail.com>
2024-06-13 13:46:17 +0700

    update: aws rotate key - add color

--- bash/scripts/awsKeyRotate.sh
+++ bash/scripts/awsKeyRotate.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
-# Check if a profile name is provided
-if [ -z "$1" ]; then
-  echo "Usage: $0 <aws-profile>"
-  exit 1
-fi
+black='\033[0;30m'
+red='\033[0;31m'
+green='\033[0;32m'
+orange='\033[0;33m'
+blue='\033[0;34m'
+purple='\033[0;35m'
+cyan='\033[0;36m'
+gray='\033[0;37m'
+gray2='\033[1;30m'
+red2='\033[1;31m'
+green2='\033[1;32m'
+yellow='\033[1;33m'
+blue2='\033[1;34m'
+purple2='\033[1;35m'
+cyan2='\033[1;36m'
+white='\033[1;37m'
+nc='\033[0m' # No Color
 
-# Define variables
-AWS_PROFILE=$1
+# Check if a profile name is provided, use 'default' if not
+AWS_PROFILE=${1:-default}
 
 # Get the current IAM user ARN
 IAM_USER_ARN=$(aws sts get-caller-identity --profile $AWS_PROFILE --query Arn --output text)
@@ -22,8 +34,8 @@ NEW_ACCESS_KEY=$(echo $NEW_KEY_INFO | jq -r '.AccessKey.AccessKeyId')
 NEW_SECRET_KEY=$(echo $NEW_KEY_INFO | jq -r '.AccessKey.SecretAccessKey')
 
 # Output the new access key information
-echo "New Access Key ID: $NEW_ACCESS_KEY"
-echo "New Secret Access Key: $NEW_SECRET_KEY"
+echo -e "New Access Key ID: ${green}$NEW_ACCESS_KEY${nc}"
+echo -e "New Secret Access Key: ${green}$NEW_SECRET_KEY${nc}"
 
 # Update the AWS CLI configuration file
 aws configure set aws_access_key_id $NEW_ACCESS_KEY --profile $AWS_PROFILE
@@ -35,4 +47,4 @@ read -p "Press [Enter] key after updating your application with the new access k
 # Delete the old access key
 aws iam delete-access-key --profile $AWS_PROFILE --access-key-id $OLD_ACCESS_KEY
 
-echo "Old access key $OLD_ACCESS_KEY has been deleted."
+echo -e "Old access key ${blue}$OLD_ACCESS_KEY${nc} has been deleted."

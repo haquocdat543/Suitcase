ddabc54a8160e6567dd19fc512af83698547e5cd
haquocdat543 <wwwdatha543@gmail.com>
2024-07-24 15:12:15 +0700

    U: script @ get latest branch commit - fix awk

--- bash/scripts/getLatestBranchCommit.sh
+++ bash/scripts/getLatestBranchCommit.sh
@@ -4,11 +4,9 @@
 LATEST_COMMIT_INFO=$(git for-each-ref --sort=-committerdate refs/heads/ --format='%(committerdate:iso8601) %(objectname) %(refname:short)' | head -n 1)
 
 # Extract the commit datetime, commit hash, and branch name
-LATEST_COMMIT_DATETIME=$(echo $LATEST_COMMIT_INFO | awk '{print $1}')
-LATEST_COMMIT_HASH=$(echo $LATEST_COMMIT_INFO | awk '{print $2}')
-LATEST_BRANCH=$(echo $LATEST_COMMIT_INFO | awk '{print $3}')
+LATEST_COMMIT_DATETIME=$(echo $LATEST_COMMIT_INFO | awk '{print $1 $2 $3}')
+LATEST_COMMIT_HASH=$(echo $LATEST_COMMIT_INFO | awk '{print $4}')
+LATEST_BRANCH=$(echo $LATEST_COMMIT_INFO | awk '{print $5}')
 
 # Print the branch with the latest commit, its hash, and datetime
-echo "Branch with the latest commit: $LATEST_BRANCH"
-echo "Latest commit hash: $LATEST_COMMIT_HASH"
-echo "Latest commit datetime: $LATEST_COMMIT_DATETIME"
+echo "Latest commit is [ ${LATEST_COMMIT_HASH} ] on branch [ ${LATEST_BRANCH} ] at [ ${LATEST_COMMIT_DATETIME} ]"

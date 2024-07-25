2917af2b87ad4ca2de70aee2774e92a9120ec89c
haquocdat543 <wwwdatha543@gmail.com>
2024-07-25 17:43:19 +0700

    U: script @ get pod resource on node - format

--- bash/scripts/getPodResourceOnNode.sh
+++ bash/scripts/getPodResourceOnNode.sh
@@ -6,8 +6,14 @@ NODE_NAME=$1
 # Get the list of pods running on the node, including their namespaces
 POD_LIST=$(kubectl get pods --all-namespaces --field-selector spec.nodeName=${NODE_NAME} -o custom-columns=NAMESPACE:.metadata.namespace,NAME:.metadata.name --no-headers)
 
+# declare result 
+RESULT=""
+
 # Loop over each pod in the list
 while read -r NAMESPACE POD; do
   # Get CPU and memory usage for each pod
-  kubectl top pod ${POD} --namespace=${NAMESPACE} --no-headers
+  RESULT+="$(kubectl top pod ${POD} --namespace=${NAMESPACE} --no-headers)\n"
 done <<< "$POD_LIST"
+
+# print out result
+echo -e ${RESULT} | column -t

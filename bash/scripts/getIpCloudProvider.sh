7a31ba75084fab6789903e4a17dc20ccc97af207
haquocdat543 <wwwdatha543@gmail.com>
2024-07-29 19:13:03 +0700

    U: script @ get ip cloud provider - add convert if it is domain

--- bash/scripts/getIpCloudProvider.sh
+++ bash/scripts/getIpCloudProvider.sh
@@ -1,10 +1,38 @@
 #!/bin/bash
 
-IPV4_ADDRESS=${1}
+# Function to check if the input is a valid IP address
+is_ip() {
+  local ip=$1
+  local stat=1
+
+  if [[ $ip =~ ^([0-9]{1,3}\.){3}[0-9]{1,3}$ ]]; then
+    OIFS=$IFS
+    IFS='.'
+    ip=($ip)
+    IFS=$OIFS
+    [[ ${ip[0]} -le 255 && ${ip[1]} -le 255 && ${ip[2]} -le 255 && ${ip[3]} -le 255 ]]
+    stat=$?
+  fi
+  return $stat
+}
+
+INPUT=$1
 
 URL="ipinfo.io"
-FULL_URL="${URL}/${IPV4_ADDRESS}"
 
-echo "Checking IP: ${FULL_URL}"
-
-curl "${FULL_URL}"
+# Check if the input is a valid IP address
+if is_ip "${INPUT}"; then
+  echo "${INPUT} is a valid IP address."
+  curl "${URL}/${INPUT}"
+else
+  echo "${INPUT} is not a valid IP address. Converting to IP address..."
+  IP_ADDRESS=$(dig +short "${INPUT}")
+  
+  if [ -z "${IP_ADDRESS}" ]; then
+    echo "Failed to resolve domain name to IP address."
+    exit 1
+  else
+    echo "The IP address for ${INPUT} is: ${IP_ADDRESS}"
+    curl "${URL}/${IP_ADDRESS}"
+  fi
+fi

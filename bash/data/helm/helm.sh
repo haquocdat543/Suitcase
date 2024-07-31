1c2c3564bc2f98f29b75fba66a21199057cf57ac
haquocdat543 <wwwdatha543@gmail.com>
2024-07-31 15:12:34 +0700

    U: data @ helm - add karpenter

--- bash/data/helm/helm.sh
+++ bash/data/helm/helm.sh
@@ -15,4 +15,5 @@ helm repo add kong https://charts.konghq.com
 helm repo add vault https://helm.releases.hashicorp.com
 helm repo add consul https://helm.releases.hashicorp.com
 helm repo add bitnami oci://registry-1.docker.io/bitnamicharts
+helm repo add karpenter oci://public.ecr.aws/karpenter/karpenter
 helm repo update

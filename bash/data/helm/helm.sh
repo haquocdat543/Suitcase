d669cc5e1dab4594b8c9a2d0bcd8a3c101f7cc5f
haquocdat543 <wwwdatha543@gmail.com>
2024-07-31 11:52:59 +0700

    A: data @ helm

---
+++ bash/data/helm/helm.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+helm repo add harbor https://helm.goharbor.io
+helm repo add istio https://istio-release.storage.googleapis.com/charts
+helm repo add kiali https://kiali.org/helm-charts
+helm repo jeknkins https://charts.jenkins.io
+

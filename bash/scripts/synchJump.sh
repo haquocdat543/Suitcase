f004d267650031bd8469b3b7dbba8311375632d6
haquocdat543 <wwwdatha543@gmail.com>
2024-08-20 14:46:26 +0700

    feat(script): synch jump - without declaration

--- bash/scripts/synchJump.sh
+++ bash/scripts/synchJump.sh
@@ -5,70 +5,15 @@
 ####################################################################################################
 # source ./file.sh
 
-# declaration function
-declaration () {
-
-  # Color declaration
-  black='\033[0;30m'
-  red='\033[0;31m'
-  green='\033[0;32m'
-  orange='\033[0;33m'
-  blue='\033[0;34m'
-  purple='\033[0;35m'
-  cyan='\033[0;36m'
-  gray='\033[0;37m'
-  gray2='\033[1;30m'
-  red2='\033[1;31m'
-  green2='\033[1;32m'
-  yellow='\033[1;33m'
-  blue2='\033[1;34m'
-  purple2='\033[1;35m'
-  cyan2='\033[1;36m'
-  white='\033[1;37m'
-  nc='\033[0m' # No Color
-
-  # Get input from user
-  read -p "$(echo -e "Enter ${yellow}JUMPHOST${nc} [${green2}notional_bastion${nc}]: ")" SERVER_JUMPHOST
-  SERVER_JUMPHOST=${SERVER_JUMPHOST:-notional_bastion}
-
-  read -p "$(echo -e "Enter ${yellow}USERNAME${nc} [${green2}root${nc}]: ")" SERVER_USERNAME
-  SERVER_USERNAME=${SERVER_USERNAME:-root}
-
-  read -p "$(echo -e "Enter ${yellow}HOSTNAME${nc} [${green2}1.1.1.1${nc}]: ")" SERVER_HOSTNAME
-  SERVER_HOSTNAME=${SERVER_HOSTNAME:-1.1.1.1}
-
-  read -p "$(echo -e "Enter ${yellow}HOSTPORT${nc} [${green2}2222${nc}]: ")" SERVER_HOSTPORT
-  SERVER_HOSTPORT=${SERVER_HOSTPORT:-2222}
-
-  read -p "$(echo -e "Enter ${yellow}KEYPATH${nc} [${green2}~/.ssh/id_rsa${nc}]: ")" KEYPATH
-  KEYPATH=${KEYPATH:-~/.ssh/id_rsa}
-
-  # Write environment to file
-cat << EOF | sudo tee env.sh
-export SERVER_JUMPHOST="${SERVER_JUMPHOST}"
-export SERVER_USERNAME="${SERVER_USERNAME}"
-export SERVER_HOSTNAME="${SERVER_HOSTNAME}"
-export SERVER_HOSTPORT="${SERVER_HOSTPORT}"
-export KEYPATH="${KEYPATH}"
-EOF
-
-}
-
-# Load or declare environment variables
-if [[ -f env.sh ]]; then
-  source env.sh
-else
-  declaration
-fi
-
 ####################################################################################################
 # GET VARIABLES
 ####################################################################################################
 
 # get variables
 OP=$1
-PATH_1=$2
-PATH_2=$3
+HOSTCONFIG=$2
+PATH_1=$3
+PATH_2=$4
 
 ####################################################################################################
 # SET FUNCTION
@@ -81,11 +26,7 @@ list_path () {
   local LOCATION_PATH=$1
 
   # logic
-  if [[ -z ${SERVER_HOSTPORT} ]]; then
-    ssh -J ${SERVER_JUMPHOST} ${SERVER_USERNAME}@${SERVER_HOSTNAME} "ls -la ${LOCATION_PATH}"
-  else
-    ssh -J ${SERVER_JUMPHOST} ${SERVER_USERNAME}@${SERVER_HOSTNAME} -p ${SERVER_HOSTPORT} "ls -la ${LOCATION_PATH}"
-  fi
+  ssh ${HOSTCONFIG} "ls -la ${LOCATION_PATH}"
 
 }
 
@@ -96,11 +37,7 @@ remove_remote () {
   local LOCATION_PATH=$1
 
   # logic
-  if [[ -z ${SERVER_HOSTPORT} ]]; then
-    ssh -J ${SERVER_JUMPHOST} ${SERVER_USERNAME}@${SERVER_HOSTNAME} "rm -rf ${LOCATION_PATH}"
-  else
-    ssh -J ${SERVER_JUMPHOST} ${SERVER_USERNAME}@${SERVER_HOSTNAME} -p ${SERVER_HOSTPORT} "rm -rf ${LOCATION_PATH}"
-  fi
+  ssh ${HOSTCONFIG} "rm -rf ${LOCATION_PATH}"
 
   echo "Successfully"
 
@@ -116,11 +53,7 @@ copy_to_remote () {
   # logic
   # scp -i ${KEYPATH} -pr "${PATH_1}" "${SERVER_USERNAME}@${SERVER_HOSTNAME}:${PATH_2}" 
   # logic
-  if [[ -z ${SERVER_HOSTPORT} ]]; then
-    rsync -av --progress -e "ssh -J ${SERVER_JUMPHOST}" "${PATH_1}" ${SERVER_USERNAME}@${SERVER_HOSTNAME}:${PATH_2}
-  else
-    rsync -av --progress -e "ssh -J ${SERVER_JUMPHOST}" "${PATH_1}" ${SERVER_USERNAME}@${SERVER_HOSTNAME}:${PATH_2} -p ${SERVER_HOSTPORT}
-  fi
+  rsync -av --progress ${PATH_1} ${HOSTCONFIG}:${PATH_2}
 
   echo "Successfully"
 
@@ -135,11 +68,7 @@ copy_to_local () {
 
   # logic
   # scp -i ${KEYPATH} "${SERVER_USERNAME}@${SERVER_HOSTNAME}:${PATH_1}" "${PATH_2}"
-  if [[ -z ${SERVER_HOSTPORT} ]]; then
-    rsync -av --progress -e "ssh -J ${SERVER_JUMPHOST}" ${SERVER_USERNAME}@${SERVER_HOSTNAME}:${PATH_1} ${PATH_2}
-  else
-    rsync -av --progress -e "ssh -J ${SERVER_JUMPHOST}" ${SERVER_USERNAME}@${SERVER_HOSTNAME}:${PATH_1} ${PATH_2} -p ${SERVER_HOSTPORT}
-  fi
+  rsync -av --progress ${HOSTCONFIG}:${PATH_1} ${PATH_2}
 
   echo "Successfully"
 
@@ -167,6 +96,9 @@ case $OP in
     ;;
   *)
     echo "Only support following operations:"
-    echo "list: list files and folders [ sh $0 list <PATH> ]"
+    echo "ls: list files and folders [ sh ${0} <HOSTCONFIG> ls <PATH> ]"
+    echo "rr: remote remote files and folders [ sh ${0} <HOSTCONFIG> rr <PATH> ]"
+    echo "cpr: copy files and folders from local to remote [ sh ${0} <HOSTCONFIG> cpr <PATH1> <PATH2> ]"
+    echo "cpl: copy files and folders from remote to local [ sh ${0} <HOSTCONFIG> rr <PATH1> <PATH2> ]"
     ;;
 esac

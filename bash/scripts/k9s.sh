2c84477a1e049ed2c341e8c3640a384dbed70fdd
haquocdat543 <wwwdatha543@gmail.com>
2024-08-27 16:37:16 +0700

    feat(script): add @ get disk size

--- bash/scripts/k9s.sh
+++ bash/scripts/k9s.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export VER="0.27.4"
+export VER="0.32.5"
 wget https://github.com/derailed/k9s/releases/download/v${VER}/k9s_Linux_amd64.tar.gz
 tar xvf k9s_Linux_amd64.tar.gz
 mv k9s /usr/local/bin/k9s

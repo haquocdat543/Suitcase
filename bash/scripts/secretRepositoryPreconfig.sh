af4f0b41636bb422e9452cc4a69151b323a3b19d
haquocdat543 <wwwdatha543@gmail.com>
2024-09-03 22:22:17 +0700

    feat(script): git secret repository - add git pull and git exist

--- bash/scripts/secretRepositoryPreconfig.sh
+++ bash/scripts/secretRepositoryPreconfig.sh
@@ -6,30 +6,65 @@ GIT_SERVER=${2}
 ORGANIZATION_OR_USER=${3}
 REPOSITORY_NAME=${4}
 
-function git::repository::clone::preconfig () {
+GIT_URL="git@${GIT_SERVER}:${ORGANIZATION_OR_USER}/${REPOSITORY_NAME}.git"
 
-  # clone operation
-  GIT_URL="git@${GIT_SERVER}:${ORGANIZATION_OR_USER}/${REPOSITORY_NAME}.git"
-  LOCAL_PATH="${HOME}/preconfig/repositories/${REPOSITORY_NAME}"
-  git clone ${GIT_URL} ${LOCAL_PATH}
+function git::repository::pull () {
+
+  DIRECTORY=${1}
+
+  cd ${DIRECTORY}
+  git reset --hard HEAD~1
+  git reset --hard HEAD~1
+  git reset --hard HEAD~1
+  git reset --hard HEAD~1
+  git reset --hard HEAD~1
+  git reset --hard HEAD~1
+  git reset --hard HEAD~1
+  git reset --hard HEAD~1
+  git reset --hard HEAD~1
+  git reset --hard HEAD~1
+  git pull origin main -f
+  cd -
+  
+}
+
+function git::repository::exist () {
+
+  DIRECTORY=${1}
+  FUNCTION=${2}
 
 }
 
-function git::repository::clone::home () {
+function git::repository::pull::preconfig () {
 
-  # clone operation
-  GIT_URL="git@${GIT_SERVER}:${ORGANIZATION_OR_USER}/${REPOSITORY_NAME}.git"
+  LOCAL_PATH="${HOME}/preconfig/repositories/${REPOSITORY_NAME}"
+
+  if [ -d "${DIRECTORY}" ]; then
+    git::repository::pull ${LOCAL_PATH}
+  else
+    git clone ${GIT_URL} ${LOCAL_PATH}
+  fi
+
+}
+
+function git::repository::pull::home () {
+
   LOCAL_PATH="${HOME}/${REPOSITORY_NAME}"
-  git clone ${GIT_URL} ${LOCAL_PATH}
+
+  if [ -d "${DIRECTORY}" ]; then
+    git::repository::pull ${LOCAL_PATH}
+  else
+    git clone ${GIT_URL} ${LOCAL_PATH}
+  fi
 
 }
 
 case ${OPTION} in
   "1")
-    git::repository::clone::preconfig ${GIT_SERVER} ${ORGANIZATION_OR_USER} ${REPOSITORY_NAME}
+    git::repository::pull::preconfig 
     ;;
   "2")
-    git::repository::clone::home ${GIT_SERVER} ${ORGANIZATION_OR_USER} ${REPOSITORY_NAME}
+    git::repository::pull::home
     ;;
   *)
     echo "Wrong option"

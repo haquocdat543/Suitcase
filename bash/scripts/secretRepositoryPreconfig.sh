8ea922c54ba2c835b0cfbdd09bb7abfd7c339e05
haquocdat543 <wwwdatha543@gmail.com>
2024-09-03 22:02:17 +0700

    feat(script): secret repository clone - add clone to home

--- bash/scripts/secretRepositoryPreconfig.sh
+++ bash/scripts/secretRepositoryPreconfig.sh
@@ -6,7 +6,7 @@ GIT_SERVER=${2}
 ORGANIZATION_OR_USER=${3}
 REPOSITORY_NAME=${4}
 
-function git::repository::clone () {
+function git::repository::clone::preconfig () {
 
   # clone operation
   GIT_URL="git@${GIT_SERVER}:${ORGANIZATION_OR_USER}/${REPOSITORY_NAME}.git"
@@ -15,9 +15,21 @@ function git::repository::clone () {
 
 }
 
+function git::repository::clone::home () {
+
+  # clone operation
+  GIT_URL="git@${GIT_SERVER}:${ORGANIZATION_OR_USER}/${REPOSITORY_NAME}.git"
+  LOCAL_PATH="${HOME}/${REPOSITORY_NAME}"
+  git clone ${GIT_URL} ${LOCAL_PATH}
+
+}
+
 case ${OPTION} in
   "1")
-    git::repository::clone ${GIT_SERVER} ${ORGANIZATION_OR_USER} ${REPOSITORY_NAME}
+    git::repository::clone::preconfig ${GIT_SERVER} ${ORGANIZATION_OR_USER} ${REPOSITORY_NAME}
+    ;;
+  "2")
+    git::repository::clone::home ${GIT_SERVER} ${ORGANIZATION_OR_USER} ${REPOSITORY_NAME}
     ;;
   *)
     echo "Wrong option"

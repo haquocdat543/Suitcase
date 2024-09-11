d0f83f8d571aa64259bd5c7a85255298855a1783
haquocdat543 <wwwdatha543@gmail.com>
2024-09-11 20:27:09 +0700

    feat(alias): get current shell

--- bash/scripts/alias.sh
+++ bash/scripts/alias.sh
@@ -28,47 +28,60 @@ git pull origin main -f
 
 cd ${CURRENT_DIRECTORY}
 
+CURRENT_SHELL=$(basename $(ps -p $$ -o "comm="))
+
+# Print current shell
+echo "Current shell: ${CURRENT_SHELL}"
+
+if [[ ${CURRENT_SHELL} == "bash" ]]; then
+   RUN_COMMAND_FILE="bashrc"
+elif [[ ${CURRENT_SHELL} == "sh" ]]; then
+   RUN_COMMAND_FILE="bashrc"
+elif [[ ${CURRENT_SHELL} == "zsh" ]]; then
+   RUN_COMMAND_FILE="zshrc"
+fi
+
 # Setup environment variables
 SSH_DIRECTORY="${HOME}/.ssh"
-BASH_RUN_COMMAND="${HOME}/.bashrc"
-DEFAULT_BASH_RUN_COMMAND="${HOME}/preconfig/Suitcase/bash/configs/default-bashrc"
-BASH_RUN_COMMAND_BACKUP="${HOME}/.bashrc.backup"
-PRECONFIG_BASH_RUN_COMMAND_BACKUP="${HOME}/preconfig/backup/.bashrc.backup "
+RUN_COMMAND="${HOME}/.${RUN_COMMAND_FILE}"
+DEFAULT_RUN_COMMAND="${HOME}/preconfig/Suitcase/bash/configs/default-${RUN_COMMAND_FILE}"
+RUN_COMMAND_BACKUP="${HOME}/.${RUN_COMMAND_FILE}.backup"
+PRECONFIG_RUN_COMMAND_BACKUP="${HOME}/preconfig/backup/.${RUN_COMMAND_FILE}.backup "
 PRECONFIG_BACKUP_DIRECTORY="${HOME}/preconfig/backup"
-BASH_RUN_COMMAND_TILDE="${HOME}/.bashrc~"
-SUITCASE_BASH_RUN_COMMAND="${HOME}/preconfig/Suitcase/bash/configs/.bashrc"
+RUN_COMMAND_TILDE="${HOME}/.${RUN_COMMAND_FILE}~"
+SUITCASE_RUN_COMMAND="${HOME}/preconfig/Suitcase/bash/configs/.${RUN_COMMAND_FILE}"
 
 ### If .ssh folder does not exist. Create it.
 mkdir -p ${SSH_DIRECTORY}
 
-### If .bashrc file does not exist. Create it and write content from "default-bashrc file to it.
-if [ ! -f ${BASH_RUN_COMMAND} ]
+### If .${RUN_COMMAND_FILE} file does not exist. Create it and write content from "default-${RUN_COMMAND_FILE} file to it.
+if [ ! -f ${RUN_COMMAND} ]
 then
-	cp -f ${BASH_RUN_COMMAND} ${DEFAULT_BASH_RUN_COMMAND}
+	cp -f ${RUN_COMMAND} ${DEFAULT_RUN_COMMAND}
 fi
 
-## If .bashrc.backup file in in root folder and preconfig/backup/.bashrc.backup folder does not exist. Create it 
-if [ ! -f ${BASH_RUN_COMMAND_BACKUP} ] && [ ! -f ${PRECONFIG_BASH_RUN_COMMAND_BACKUP} ]
+## If .${RUN_COMMAND_FILE}.backup file in in root folder and preconfig/backup/.${RUN_COMMAND_FILE}.backup folder does not exist. Create it 
+if [ ! -f ${RUN_COMMAND_BACKUP} ] && [ ! -f ${PRECONFIG_RUN_COMMAND_BACKUP} ]
 then
-	cp -f ${BASH_RUN_COMMAND} ${BASH_RUN_COMMAND_BACKUP}
+	cp -f ${RUN_COMMAND} ${RUN_COMMAND_BACKUP}
 fi
 
-## If .bashrc~ exist. Delete it 
-rm -rf ${BASH_RUN_COMMAND_TILDE}
+## If .${RUN_COMMAND_FILE}~ exist. Delete it 
+rm -rf ${RUN_COMMAND_TILDE}
 
-mv -f ${BASH_RUN_COMMAND} ${BASH_RUN_COMMAND_TILDE}
+mv -f ${RUN_COMMAND} ${RUN_COMMAND_TILDE}
 
-sed '/^alias/d' ${BASH_RUN_COMMAND_TILDE} | sed '/^$/d' | sed '/^##/d' | tee ${BASH_RUN_COMMAND}
+sed '/^alias/d' ${RUN_COMMAND_TILDE} | sed '/^$/d' | sed '/^##/d' | tee ${RUN_COMMAND}
 
 ## If preconfig/backup folder does not exist. Create it 
 mkdir -p ${PRECONFIG_BACKUP_DIRECTORY}
 
-## If .bashrc.backup file in in preconfig/backup folder does not exist. Copy from root folder to.
-if [ ! -f ${PRECONFIG_BASH_RUN_COMMAND_BACKUP} ]
+## If .${RUN_COMMAND_FILE}.backup file in in preconfig/backup folder does not exist. Copy from root folder to.
+if [ ! -f ${PRECONFIG_RUN_COMMAND_BACKUP} ]
 then
-	mv -f ${BASH_RUN_COMMAND_BACKUP} ${PRECONFIG_BASH_RUN_COMMAND_BACKUP}
+	mv -f ${RUN_COMMAND_BACKUP} ${PRECONFIG_RUN_COMMAND_BACKUP}
 fi
-## If .bashrc.backup file in in preconfig/backup folder does not exist. Copy from root folder to.
-rm -rf ${BASH_RUN_COMMAND_BACKUP}
+## If .${RUN_COMMAND_FILE}.backup file in in preconfig/backup folder does not exist. Copy from root folder to.
+rm -rf ${RUN_COMMAND_BACKUP}
 
-cat ${SUITCASE_BASH_RUN_COMMAND} | tee -a ${BASH_RUN_COMMAND} ; clear ; source ${BASH_RUN_COMMAND}
+cat ${SUITCASE_RUN_COMMAND} | tee -a ${RUN_COMMAND} ; clear ; source ${RUN_COMMAND}

45586634c99667e9f3c01ac01c62ba772f0c17f5
haquocdat543 <wwwdatha543@gmail.com>
2024-09-11 20:18:10 +0700

    refactor: alias

--- bash/scripts/alias.sh
+++ bash/scripts/alias.sh
@@ -1,66 +1,74 @@
+#!/bin/bash
+
+# Update and install git for CentOS
 yum update -y
-yum install sudo -y
 yum install -y git
+
+# Update and install git for Ubuntu
 apt update -y
-apt instal -y sudo 
 apt install -y git
+
+# Update and install git for Archlinux
 pacman -Syu --noconfirm
-pacman -Sy --noconfirm sudo
 pacman -Sy --noconfirm git
-git clone https://github.com/haquocdat543/Suitcase.git $HOME/preconfig/Suitcase
-cd $HOME/preconfig/Suitcase
-git reset --hard HEAD~1
-git reset --hard HEAD~1
-git reset --hard HEAD~1
-git reset --hard HEAD~1
-git reset --hard HEAD~1
-git reset --hard HEAD~1
-git reset --hard HEAD~1
-git reset --hard HEAD~1
-git reset --hard HEAD~1
-git reset --hard HEAD~1
+
+# Set up environment variables
+GIT_REMOTE_URL="https://github.com/haquocdat543/Suitcase.git"
+
+DESTINATION_DIRECTORY="${HOME}/Suitcase"
+
+# Clone git suitcase repository
+git clone ${GIT_REMOTE_URL} ${DESTINATION_DIRECTORY}
+
+CURRENT_DIRECTORY=$(pwd)
+cd ${DESTINATION_DIRECTORY}
+
+git reset --hard HEAD~10
 git pull origin main -f
-cd -
+
+cd ${CURRENT_DIRECTORY}
+
+# Setup environment variables
+SSH_DIRECTORY="${HOME}/.ssh"
+BASH_RUN_COMMAND="${HOME}/.bashrc"
+DEFAULT_BASH_RUN_COMMAND="${HOME}/preconfig/Suitcase/bash/configs/default-bashrc"
+BASH_RUN_COMMAND_BACKUP="${HOME}/.bashrc.backup"
+PRECONFIG_BASH_RUN_COMMAND_BACKUP="${HOME}/preconfig/backup/.bashrc.backup "
+PRECONFIG_BACKUP_DIRECTORY="${HOME}/preconfig/backup"
+BASH_RUN_COMMAND_TILDE="${HOME}/.bashrc~"
+SUITCASE_BASH_RUN_COMMAND="${HOME}/preconfig/Suitcase/bash/configs/.bashrc"
+
 ### If .ssh folder does not exist. Create it.
-if [ ! -d $HOME/.ssh ]
-then
-	mkdir -f $HOME/.ssh
-fi
+mkdir -p ${SSH_DIRECTORY}
+
 ### If .bashrc file does not exist. Create it and write content from "default-bashrc file to it.
-if [ ! -f $HOME/.bashrc ]
+if [ ! -f ${BASH_RUN_COMMAND} ]
 then
-	cp -f $HOME/preconfig/Suitcase/bash/configs/default-bashrc $HOME/.bashrc
+	cp -f ${BASH_RUN_COMMAND} ${DEFAULT_BASH_RUN_COMMAND}
 fi
+
 ## If .bashrc.backup file in in root folder and preconfig/backup/.bashrc.backup folder does not exist. Create it 
-if [ ! -f $HOME/.bashrc.backup ] && [ ! -f $HOME/preconfig/backup/.bashrc.backup ]
+if [ ! -f ${BASH_RUN_COMMAND_BACKUP} ] && [ ! -f ${PRECONFIG_BASH_RUN_COMMAND_BACKUP} ]
 then
-	cp -f $HOME/.bashrc $HOME/.bashrc.backup 
+	cp -f ${BASH_RUN_COMMAND} ${BASH_RUN_COMMAND_BACKUP}
 fi
 
 ## If .bashrc~ exist. Delete it 
-if [ -f $HOME/bashrc~ ]
-then
-	rm -f $HOME/.bashrc~
-fi
+rm -rf ${BASH_RUN_COMMAND_TILDE}
 
-mv -f $HOME/.bashrc $HOME/.bashrc~
+mv -f ${BASH_RUN_COMMAND} ${BASH_RUN_COMMAND_TILDE}
 
-sed '/^alias/d' $HOME/.bashrc~ | sed '/^$/d' | sed '/^##/d' | tee $HOME/.bashrc
+sed '/^alias/d' ${BASH_RUN_COMMAND_TILDE} | sed '/^$/d' | sed '/^##/d' | tee ${BASH_RUN_COMMAND}
 
 ## If preconfig/backup folder does not exist. Create it 
-if [ ! -d $HOME/preconfig/backup ]
-then
-	mkdir $HOME/preconfig/backup 
-fi
-## If .bashrc.backup file in in preconfig/backup folder does not exist. Copy from root folder to.
-if [ ! -f $HOME/preconfig/backup/.bashrc.backup ]
-then
-	mv -f $HOME/.bashrc.backup $HOME/preconfig/backup/.bashrc.backup 
-fi
-## If .bashrc.backup file in in preconfig/backup folder does not exist. Copy from root folder to.
-if [ -f $HOME/.bashrc.backup ]
-then
-	rm -f $HOME/.bashrc.backup 
-fi
+mkdir -p ${PRECONFIG_BACKUP_DIRECTORY}
 
-cat $HOME/preconfig/Suitcase/bash/configs/.bashrc | tee -a $HOME/.bashrc ; clear ; source $HOME/.bashrc
+## If .bashrc.backup file in in preconfig/backup folder does not exist. Copy from root folder to.
+if [ ! -f ${PRECONFIG_BASH_RUN_COMMAND_BACKUP} ]
+then
+	mv -f ${BASH_RUN_COMMAND_BACKUP} ${PRECONFIG_BASH_RUN_COMMAND_BACKUP}
+fi
+## If .bashrc.backup file in in preconfig/backup folder does not exist. Copy from root folder to.
+rm -rf ${BASH_RUN_COMMAND_BACKUP}
+
+cat ${SUITCASE_BASH_RUN_COMMAND} | tee -a ${BASH_RUN_COMMAND} ; clear ; source ${BASH_RUN_COMMAND}
--- bash/scripts/alias2.sh
+++ bash/scripts/alias2.sh
@@ -1,20 +1,29 @@
+#!/bin/bash
+
+# Update and install git for CentOS
 yum update -y
 yum install -y git
+
+# Update and install git for Ubuntu
 apt update -y
 apt install -y git
+
+# Update and install git for Archlinux
 pacman -Syu --noconfirm
 pacman -Sy --noconfirm git
-git clone git@github.com/haquocdat543/Suitcase.git $HOME/Suitcase
-cd $HOME/Suitcase
-git reset --hard HEAD~1
-git reset --hard HEAD~1
-git reset --hard HEAD~1
-git reset --hard HEAD~1
-git reset --hard HEAD~1
-git reset --hard HEAD~1
-git reset --hard HEAD~1
-git reset --hard HEAD~1
-git reset --hard HEAD~1
-git reset --hard HEAD~1
+
+# Set up environment variables
+GIT_REMOTE_URL="git@github.com/haquocdat543/Suitcase.git"
+
+DESTINATION_DIRECTORY="${HOME}/Suitcase"
+
+# Clone git suitcase repository
+git clone ${GIT_REMOTE_URL} ${DESTINATION_DIRECTORY}
+
+CURRENT_DIRECTORY=$(pwd)
+cd ${DESTINATION_DIRECTORY}
+
+git reset --hard HEAD~10
 git pull origin main -f
-cd -
+
+cd ${CURRENT_DIRECTORY}

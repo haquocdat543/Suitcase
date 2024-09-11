ddbc24aa0f499ae77aefae443dfc22b50f1ed2a8
haquocdat543 <wwwdatha543@gmail.com>
2024-09-11 21:01:42 +0700

    feat(script): current shell regex pattern

--- bash/scripts/alias.sh
+++ bash/scripts/alias.sh
@@ -35,11 +35,11 @@ CURRENT_SHELL=$(basename $(ps -p $$ -o "comm="))
 # Print current shell
 echo "Current shell: ${CURRENT_SHELL}"
 
-if [[ ${CURRENT_SHELL} == "bash" ]]; then
+if [[ ${CURRENT_SHELL} =~ "bash" ]]; then
    RUN_COMMAND_FILE="bashrc"
-elif [[ ${CURRENT_SHELL} == "sh" ]]; then
+elif [[ ${CURRENT_SHELL} =~ "sh" ]]; then
    RUN_COMMAND_FILE="bashrc"
-elif [[ ${CURRENT_SHELL} == "zsh" ]]; then
+elif [[ ${CURRENT_SHELL} =~ "zsh" ]]; then
    RUN_COMMAND_FILE="zshrc"
 fi
 
--- bash/scripts/sourceRunCommand.sh
+++ bash/scripts/sourceRunCommand.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
+set -x
+
 CURRENT_SHELL=$(basename $(ps -p $$ -o "comm="))
 
 # Print current shell
 echo "Current shell: ${CURRENT_SHELL}"
 
-if [[ ${CURRENT_SHELL} == "bash" ]]; then
+if [[ ${CURRENT_SHELL} =~ bash ]]; then
    RUN_COMMAND_FILE="bashrc"
-elif [[ ${CURRENT_SHELL} == "sh" ]]; then
+elif [[ ${CURRENT_SHELL} =~ sh ]]; then
    RUN_COMMAND_FILE="bashrc"
-elif [[ ${CURRENT_SHELL} == "zsh" ]]; then
+elif [[ ${CURRENT_SHELL} =~ zsh ]]; then
    RUN_COMMAND_FILE="zshrc"
 fi
 
 RC_FILE=".${RUN_COMMAND_FILE}"
-source ${RC_FILE}
+source ${HOME}/${RC_FILE}
+
+set +x

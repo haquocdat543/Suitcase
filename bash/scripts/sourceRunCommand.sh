651cd451010c9b1a6fcb5c466c1fcf59f3b83639
haquocdat543 <wwwdatha543@gmail.com>
2024-09-13 11:20:50 +0700

    feat(script): source run command - upgrade

--- bash/scripts/sourceRunCommand.sh
+++ bash/scripts/sourceRunCommand.sh
@@ -2,16 +2,20 @@
 
 set -x
 
+# Get the current shell command (e.g., "-bash", "zsh", etc.)
 CURRENT_SHELL=$(ps -p $$ -o "comm=")
 
-if [[ ${CURRENT_SHELL} == "-bash" ]]; then
-   CURRENT_SHELL="bash"
+# If CURRENT_SHELL starts with "-", strip the leading "-"
+if [[ ${CURRENT_SHELL} == -* ]]; then
+   CURRENT_SHELL="${CURRENT_SHELL#-}"
+fi
 
-BASE_CURRENT_SHELL=$(basename ${CURRENT_SHELL})
+BASE_CURRENT_SHELL=$(basename "${CURRENT_SHELL}")
 
 # Print current shell
 echo "Current shell: ${BASE_CURRENT_SHELL}"
 
+# Select the appropriate RC file based on the shell
 if [[ ${BASE_CURRENT_SHELL} == "bash" ]]; then
    RUN_COMMAND_FILE=".bashrc"
 elif [[ ${BASE_CURRENT_SHELL} == "sh" ]]; then
@@ -20,6 +24,7 @@ elif [[ ${BASE_CURRENT_SHELL} == "zsh" ]]; then
    RUN_COMMAND_FILE=".zshrc"
 fi
 
-source ${HOME}/${RUN_COMMAND_FILE}
+# Source the appropriate RC file
+source "${HOME}/${RUN_COMMAND_FILE}"
 
 set +x

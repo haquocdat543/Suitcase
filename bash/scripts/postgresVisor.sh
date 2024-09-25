d846c8e37844104a0e85e813dd963fd96059b1e7
haquocdat543 <wwwdatha543@gmail.com>
2024-09-25 17:41:07 +0700

    feat(script): postgres visor

---
+++ bash/scripts/postgresVisor.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+OPERATION=${1}
+
+read -r -d '' YAML_CONFIG <<EOF
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: postgres-visor
+  labels:
+    app: postgres-visor
+spec:
+  replicas: 1
+  revisionHistoryLimit: 10
+  strategy: # change here
+    type: RollingUpdate # you can choose Recreate instead of RollingUpdate
+  selector:
+    matchLabels:
+      app: postgres-visor
+  template:
+    metadata:
+      labels:
+        app: postgres-visor
+    spec:
+      containers:
+      - name: postgres-visor
+        image: postgres:latest
+EOF
+
+echo "${YAML_CONFIG}" | kubectl ${OPERATION} -f - 

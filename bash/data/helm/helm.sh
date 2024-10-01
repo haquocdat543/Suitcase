d252e9567252a43351e130c5d3e7514c2981f7cf
haquocdat543 <wwwdatha543@gmail.com>
2024-10-01 11:08:30 +0700

    feat(data): helm - add sonarqube

--- bash/data/helm/helm.sh
+++ bash/data/helm/helm.sh
@@ -17,5 +17,6 @@ helm repo add consul https://helm.releases.hashicorp.com
 helm repo add bitnami oci://registry-1.docker.io/bitnamicharts
 helm repo add karpenter oci://public.ecr.aws/karpenter/karpenter
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo add sonarqube https://SonarSource.github.io/helm-chart-sonarqube
 helm repo add kedacore https://kedacore.github.io/charts
 helm repo update

a61e36ef40b22c7c3fb9ed5c1b3c5f72576981a0
haquocdat543 <wwwdatha543@gmail.com>
2024-10-30 10:22:03 +0700

    script: update kube ssh

--- bash/scripts/kubeSsh.sh
+++ bash/scripts/kubeSsh.sh
@@ -4,6 +4,12 @@
 GITHUB_URL="https://github.com/luksa/kubectl-plugins"
 LOCAL_PATH="$HOME/kubectl-plugins"
 
-# clone and add to PATH
-git clone ${GITHUB_URL} ${LOCAL_PATH}
-export PATH="${PATH}:${LOCAL_PATH}"
+# check repository exist
+if [[ ! -d ${LOCAL_PATH} ]]; then
+	git clone ${GITHUB_URL} ${LOCAL_PATH}
+	export PATH="${PATH}:${LOCAL_PATH}"
+else
+	cd ${LOCAL_PATH}
+	git pull
+	export PATH="${PATH}:${LOCAL_PATH}"
+fi
